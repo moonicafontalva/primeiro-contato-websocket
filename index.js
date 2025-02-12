055604940e660ff22f02a82266c204882c98cd29
Mônica <monicafontalva@hotmail.com>
2025-02-12 10:48:46 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const http = require('http');
+
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+
+const io = new Server(server);
+
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(__dirname + '/index.html')
+});
+io.on('connection', (socket) => {
+    console.log('alguém se conectou com o id' +  socket.id)
+})
+
+function geraValor() {
+    return (Math.random() * 100).toFixed(2)
+}
+
+setInterval(() => {
+    io.emit('cotação', geraValor())
+}, 1000)
+
+
+server.listen(3000, () => {
+    console.log('ouvindo na porta 3000')
+})
